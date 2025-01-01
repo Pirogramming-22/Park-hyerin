5eefd05c6397dd52f71420ce758831fe73ea1687
unknown <phl2510@naver.com>
2025-01-01 16:10:55 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -4,11 +4,27 @@ function loadItems(){
         .then(response => response.json())
         .then(json => json.items);
 }
+
+//Update the list with the given items
+function displayItems(items){
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+//Create HTML list item from the given data item
+function createHTMLString(item){
+    return `
+        <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+            <span class="item_description">${item.gender}, ${item.size}</span>
+        </li>
+        `;
+}
+
 //main
 loadItems()
     .then(items => {
-        console.log(items);
-        //displayItems(items);
+        displayItems(items);
         //setEventListeners(items)
     })
-.catch(console.log);
+    .catch(console.log);
