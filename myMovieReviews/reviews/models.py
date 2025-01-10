8e5905f8eabbd7d6d2c5942dbe15288811eff738
unknown <phl2510@naver.com>
2025-01-10 14:11:09 +0900

    기본 기능 및 챌린지 구현, 리드미 생성

--- myMovieReviews/reviews/models.py
+++ myMovieReviews/reviews/models.py
@@ -2,10 +2,23 @@ from django.db import models
 
 # Create your models here.
 class MovieReview(models.Model):
+    GENRE_CHOICES = [
+        ('action', '액션'),
+        ('comedy', '코미디'),
+        ('drama', '드라마'),
+        ('horror', '공포'),
+        ('fantasy', '판타지'),
+        ('SF', '공상과학'),
+        ('musical', '뮤지컬'),
+        ('history', '역사'),
+        ('mystery', '미스터리'),
+        ('documentary', '다큐'),
+        ('animation', '애니메이션')
+    ]
     title = models.CharField(max_length=100)
     director = models.CharField(max_length=100)
     actors = models.CharField(max_length=255)
-    genre = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50, choices=GENRE_CHOICES, verbose_name="장르르" )
     rating = models.DecimalField(max_digits=2, decimal_places=1)
     runtime = models.PositiveIntegerField()  # minutes
     review_content = models.TextField()
--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -6,12 +6,22 @@ from .forms import MovieReviewForm
 
 # 리뷰 리스트 페이지
 def post_list(request):
-    reviews = MovieReview.objects.all()
-    return render(request, 'reviews/post_list.html', {'reviews': reviews})
+    sort_by = request.GET.get('sort', 'title')
+    if sort_by in ['rating', 'runtime']:
+        sort_by = f'-{sort_by}'
+    reviews = MovieReview.objects.all().order_by(sort_by)
+    for review in reviews:
+        hours = review.runtime // 60
+        minutes = review.runtime % 60
+        review.runtime_display = f"{hours}시간 {minutes}분"
+    return render(request, 'reviews/post_list.html', {'reviews': reviews, 'sort_by': sort_by})
 
 # 리뷰 디테일 페이지
 def post_detail(request, pk):
     review = get_object_or_404(MovieReview, pk=pk)
+    hours = review.runtime // 60  # 시간 계산
+    minutes = review.runtime % 60  # 분 계산
+    review.runtime_display = f"{hours}시간 {minutes}분"
     return render(request, 'reviews/post_detail.html', {'review': review})
 
 # 리뷰 작성 페이지
