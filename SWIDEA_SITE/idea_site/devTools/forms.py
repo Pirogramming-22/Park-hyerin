6750f24fc8523d225b3cd69c5f4942cb7b22bcfd
unknown <phl2510@naver.com>
2025-01-16 07:56:13 +0900

    css

--- SWIDEA_SITE/idea_site/devTools/forms.py
+++ SWIDEA_SITE/idea_site/devTools/forms.py
@@ -5,3 +5,8 @@ class DevToolForm(forms.ModelForm):
     class Meta:
         model = DevTool
         fields = ['name', 'kind', 'content']
+        labels = {
+            'name': '이름',  
+            'kind': '종류',   
+            'content': '개발툴 설명',  
+        }
